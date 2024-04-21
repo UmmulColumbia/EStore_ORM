82b9292d15a97e3305835681daf86966553c4164
Ummul Mukta <ummulcolumbia@gmail.com>
2024-04-21 13:17:58 -0400

    new data has been created in postgres

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection'); // Make sure this path is correct
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   }
-);
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'category'  // Ensure the table name is correct in your DB
+});
 
 module.exports = Category;
